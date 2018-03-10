76fb6c5259ddb464f87712634945c155b9509a94
Mits <Mits@DESKTOP-PBMLLJ9>
2018-03-10 21:10:20 +0100

    asd

--- Lekcja2/Lekcja2/Program.cs
+++ Lekcja2/Lekcja2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+public class Point
+{
+    public int x, y;
+}
+class Test
+{
+    static void Main()
+    {
+        Point p = null;
+        Console.WriteLine(p == null);
+        Console.ReadKey();
+    }
+}
