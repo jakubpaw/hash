72206c18f2cd37e8718554905eb541c72aa4ef9f
Mits <Mits@DESKTOP-PBMLLJ9>
2018-03-10 19:57:42 +0100

    lekcja1

---
+++ Lekcja1/Lekcja1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class UnitConverter
+{
+    int ratio;
+    public UnitConverter(int unitRatio) { ratio = unitRatio; }
+    public int converter(int unit) { return unit * ratio; }
+}
+class Test
+{
+    static void Main()
+    {
+        UnitConverter feetToInchesConverter = new UnitConverter(12);
+        UnitConverter milesToFeetConverter = new UnitConverter(5289);
+        Console.WriteLine(feetToInchesConverter.converter(10));
+        Console.WriteLine(milesToFeetConverter.converter(10));
+        Console.ReadKey();
+    }
+}
